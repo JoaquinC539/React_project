253db3bcab87d4f842b509cae63c0f1bb1eab8bb
JoaquinC539 <cjoaquinangel@gmail.com>
2023-03-31 15:16:47 -0600

    completed fetch items

--- firstproject/src/Content.js
+++ firstproject/src/Content.js
@@ -1,13 +1,13 @@
 import ItemList from "./ItemList"
 const Content=({items, handleCheck,handleDelete})=>{
     return (
-        <main>
+        <>
           {items.length?(
             <ItemList  items={items} handleCheck={handleCheck} handleDelete={handleDelete}/>
           ):(
             <p style={{marginTop:'rem'}}>Your list is empty</p>
           )}
-        </main>
+        </>
     )
 }
 export default Content
